cc28b174c8f08fdd5b717e11c27a8720a1b1c9e8
Jonathan Koh <jonathan.koh75@gmail.com>
2022-12-22 17:48:42 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import './App.css';
-import Navbar from './navbar';
-import Home from './components/home';
+import Navbar from './components/navbar';
 import Counter from './components/counter';
-import Projects from './components/projects';
-import Donate from './components/donate';
 
 
 function App() {
-
-
-  let component;
-  switch(window.location.pathname){
-    case "/":
-      component = <Home />
-      break
-    case "/home":
-      component = <Home />
-      break
-    case "/counter":
-      component = <Counter />
-      break
-    case "/projects":
-      component = <Projects />
-      break
-    case "/donate":
-      component = <Donate />
-      break
-  }
 
   return (
     <div className="App">
       <div className="container">
         <Navbar />
-        <div className="contentContainer">
-          {component}
-        </div>
+        <Counter />
       </div>
     </div>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+    return (
+      <div className="navbar">
+        <h1>Home Counter</h1>
+        <ul>
+            <li>Counter</li>
+            <li>Projects</li>
+            <li>Donate</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Navbar;
